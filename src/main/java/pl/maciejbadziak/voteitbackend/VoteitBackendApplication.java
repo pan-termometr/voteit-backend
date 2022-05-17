0d3a4e65df360f45a1f18b49462b71e9d967e285
pan-termometr <termomemtr@gmail.com>
2022-05-17 23:28:17 +0200

    initial commit

---
+++ src/main/java/pl/maciejbadziak/voteitbackend/VoteitBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.maciejbadziak.voteitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteitBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/maciejbadziak/voteitbackend/VoteitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.maciejbadziak.voteitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
