29e68700ce2039708dcb51b192a7733cf524c291
pan-termometr <termomemtr@gmail.com>
2022-06-05 13:35:24 +0200

    [main] removed voteits from TagDto

--- src/main/java/pl/maciejbadziak/voteitbackend/dto/TagDto.java
+++ src/main/java/pl/maciejbadziak/voteitbackend/dto/TagDto.java
@@ -2,15 +2,11 @@ package pl.maciejbadziak.voteitbackend.dto;
 
 import lombok.Data;
 import org.springframework.stereotype.Component;
-import pl.maciejbadziak.voteitbackend.entity.Voteit;
-
-import java.util.Set;
 
 @Component
 @Data
 public class TagDto {
 
     private Long id;
-    private Set<Voteit> voteits;
     private String name;
 }
